25b726bc22f92fc97c77efff2104a57b9889d76f
ijungleboy <daniel.mettler@2sic.com>
2021-10-13 14:18:53 +0200

    minor

--- staging/api/Parts/SendMail.cs
+++ staging/api/Parts/SendMail.cs
@@ -15,27 +15,20 @@ public class SendMail : Custom.Hybrid.Code12
     var workflow = AsList(App.Data["Workflow"]).Where(w => w.WorkflowId == workflowId).FirstOrDefault();
 
     // rewrite the keys to be a nicer format, based on the configuration
-    var valuesWithMailLabels = RewriteKeys(contactFormRequest, workflow.MailLabels ?? "");
+    var valuesRelabled = RewriteKeys(contactFormRequest, workflow.MailLabels ?? "");
 
     // assemble all settings to send the mail
-    // background: some settings are made in this module,
-    // but if they are missing we use fallback settings
-    // which are taken from the Settings
-    var settings = new
-    {
-      MailFrom = !String.IsNullOrEmpty(Content.MailFrom) ? Content.MailFrom : Settings.DefaultMailFrom,
-      OwnerMail = !String.IsNullOrEmpty(Content.OwnerMail) ? Content.OwnerMail : Settings.DefaultOwnerMail
-    };
+    // background: some settings are made in this module, but if they are missing we use fallback settings
+    var from = Text.Has(Content.MailFrom) ? Content.MailFrom : Settings.DefaultMailFrom;
+    var owner = Text.Has(Content.OwnerMail) ? Content.OwnerMail : Settings.DefaultOwnerMail;
 
     // Send Mail to owner
-    if (Content.OwnerSend != null && Content.OwnerSend)
+    if (Content.OwnerSend == true)
     {
       Log.Add("Send Mail to Owner");
       try
       {
-        Send(
-          workflow.OwnerMailTemplate, valuesWithMailLabels, settings.MailFrom, settings.OwnerMail, Content.OwnerMailCC, custMail, files
-        );
+        Send(workflow.OwnerMailTemplate, valuesRelabled, from, owner, Content.OwnerMailCC, custMail, files);
       }
       catch (Exception ex)
       {
@@ -44,14 +37,12 @@ public class SendMail : Custom.Hybrid.Code12
     }
 
     // Send Mail to customer
-    if (Content.CustomerSend != null && Content.CustomerSend && !String.IsNullOrEmpty(custMail))
+    if (Content.CustomerSend == true && Text.Has(custMail))
     {
       Log.Add("Send Mail to Customer");
       try
       {
-        Send(
-          workflow.CustomerMailTemplate, valuesWithMailLabels, settings.MailFrom, custMail, Content.CustomerMailCC, settings.OwnerMail, files
-        );
+        Send(workflow.CustomerMailTemplate, valuesRelabled, from, custMail, Content.CustomerMailCC, owner, files);
       }
       catch (Exception ex)
       {
@@ -60,48 +51,33 @@ public class SendMail : Custom.Hybrid.Code12
     }
   }
 
-  public void Send(
-    string emailTemplateFilename,
-    Dictionary<string, object> valuesWithMailLabels,
-    string MailFrom,
-    string MailTo,
-    string MailCC,
-    string MailReply,
-    List<ToSic.Sxc.Adam.IFile> files)
+  public void Send(string emailTemplateFilename, Dictionary<string, object> valuesWithMailLabels, 
+    string from, string to, string cc, string replyTo, List<ToSic.Sxc.Adam.IFile> files)
   {
     // Log what's happening in case we run into problems
-    var wrapLog = Log.Call("template:" + emailTemplateFilename + ", from:" + MailFrom + ", to:" + MailTo + ", cc:" + MailCC + ", reply:" + MailReply);
+    var wrapLog = Log.Call("template:" + emailTemplateFilename + ", from:" + from + ", to:" + to + ", cc:" + cc + ", reply:" + replyTo);
 
     Log.Add("Get MailEngine");
     var mailEngine = CreateInstance("../../email-templates/" + emailTemplateFilename);
     var mailBody = mailEngine.Message(valuesWithMailLabels).ToString();
-    var mailSubj = mailEngine.Subject(valuesWithMailLabels);
+    var subject = mailEngine.Subject(valuesWithMailLabels);
 
     // Send Mail
     // Note that if an error occurs, this will bubble up, the caller will convert it to format for the client
     Log.Add("sending...");
     var mailService = GetService<IMailService>();
-    mailService.Send(
-      from: MailFrom,
-      to: MailTo,
-      cc: MailCC,
-      bcc: "",
-      replyTo: MailReply,
-      subject: mailSubj,
-      body: mailBody,
-      attachments: files);
+    mailService.Send(from: from, to: to, cc: cc, replyTo: replyTo, subject: subject, body: mailBody, attachments: files);
 
     // Log to Platform - just as a last resort in case something is lost, to track down why
     var message = new StringBuilder()
-      .AppendLine("MailFrom: " + MailFrom)
-      .AppendLine("MailTo: "+ MailTo)
-      .AppendLine("MailCC: "+ MailCC)
-      .AppendLine("MailReply: "+ MailReply)
-      .AppendLine("MailSubject: "+ mailSubj)
+      .AppendLine("MailFrom: " + from)
+      .AppendLine("MailTo: "+ to)
+      .AppendLine("MailCC: "+ cc)
+      .AppendLine("MailReply: "+ replyTo)
+      .AppendLine("MailSubject: "+ subject)
       .ToString();
 
-    var platformLogService = GetService<ILogService>();
-    platformLogService.Add("SendMail", message.ToString());
+    GetService<ILogService>().Add("SendMail", message);
 
     wrapLog("ok");
   }
