7eed7ca949121b1d1264272468bff33fd1a10733
Maxim Hüsler <maxim.huesler@2sic.com>
2021-11-05 09:32:18 +0100

    minor fix helpers

--- staging/shared/Helpers.cs
+++ staging/shared/Helpers.cs
@@ -3,6 +3,6 @@ using ToSic.Razor.Blade;
 public class Helpers: Custom.Hybrid.Code12
 {
   public string WrapperClasses(dynamic data) { 
-    return "app-mobius5-wrapper" + (data.Mailchimp ?? false ? "app-mobius5-mailchimp" : "");
+    return "app-mobius5-wrapper" + (data.Mailchimp ?? false ? " app-mobius5-mailchimp" : "");
   }
 }
