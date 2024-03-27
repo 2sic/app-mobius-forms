0ee86d6d615d802753210b6c0881a53c40a1742e
Roman Opalko <roman@opalko.de>
2024-03-27 07:54:29 +0100

    bootstrap update

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = (env) => {
   return {
     entry: {
-      styles: `./src/styles/bs4.scss`,
+      styles: `./src/styles/bs5.scss`,
       scripts: "./src/ts/index.ts",
     },
     output: {
